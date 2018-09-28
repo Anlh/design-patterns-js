d15857dab7d26e35ee44b4e830a8b9cc0e042536
Anlh <heldern19@gmail.com>
2018-09-28 17:41:49 +0100

    -> Implementation of cat clicker premium requirement

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # design-patterns-js
 Step by step project that shows how important is using design patterns in our code
 
-<h2>Oh gosh! Our first requirements change! </h2>
+<h2>A new requirement!</h2>
 
-<p>Since requirements are always changing, (needs text....) </p>
+<p><strong>Cat click premium</strong> is the ultimate feature that all the cat lovers were asking for!</p>
 
-<h2>New Cat Clicker Project requirements</h2>
+<h2>Cat click premium Project requirements</h2>
+
+<h3>Visually</h3>
 <ul>
-    <li>2 Cat images instead of only one</li>
-    <li>Name of the cat</li>
-    <li>Text showing the number of clicks for each cat clicked</li>
+    <li>List of 5 cats with their names</li>
+    <li>Area to display the cat selected from the list</li>
+    <li>In the cat's area it must be displayed the name, image and the text showing the number of times the cat was clicked</li>
 </ul>
--- app.js
+++ app.js
@@ -1,18 +1,40 @@
-const $catImgEl = $('#cat-img-js');
-const $catSpanEl = $('#cat-counter-js');
-const $catImg2El = $('#cat2-img-js');
-const $catSpan2El = $('#cat2-counter-js');
+const $catUlListEl = $('.cat-ul-list');
+const $catDisplayEl = $('.cat-display-area');
 
-let numberClicks = 0;
-$catSpanEl.text(numberClicks);
+const cats = [
+    {name: 'Thomas', img: 'https://purr.objects-us-west-1.dream.io/i/PtRBa.jpg', clicksNum: 0},
+    {name: 'Jefferson', img: 'https://purr.objects-us-west-1.dream.io/i/s6LNd.jpg', clicksNum: 0},
+    {name: 'Donald Trump', img: 'https://purr.objects-us-west-1.dream.io/i/xrM9q.jpg', clicksNum: 0},
+    {name: 'Cristiano Ronaldo', img: 'https://purr.objects-us-west-1.dream.io/i/BaO8d.jpg', clicksNum: 0},
+    {name: 'Mia', img: 'https://purr.objects-us-west-1.dream.io/i/x6SRI.jpg', clicksNum: 0},
+];
 
-$catImgEl.on('click', () => {
-    $catSpanEl.text(++numberClicks);
+cats.forEach((cat, index) => {
+    $('<li/>', {
+        text: cat.name,
+        "data-cat-id": `${index}`,
+    }).appendTo($catUlListEl);
 });
 
-let numberClicks2 = 0;
-$catSpan2El.text(numberClicks2);
 
-$catImg2El.on('click', () => {
-    $catSpan2El.text(++numberClicks2);
+$catUlListEl.on('click', 'li', event => {
+    const $currentCat = $(event.currentTarget);
+    const catId = $currentCat.data('catId');
+    const catObj = cats[catId];
+
+    $catDisplayEl.html(
+        `<h2>${catObj.name}</h2>
+        <img data-cat-id="${catId}" width="400px" src="${ catObj.img}"><br>
+        <span class="cat-counter">${catObj.clicksNum}</span> Meows!`
+    );
+});
+
+$catDisplayEl.on('click', 'img', (event) => {
+    const $currentCat = $(event.currentTarget);
+    const catId = $currentCat.data('catId');
+    const catObj = cats[catId];
+
+    catObj.clicksNum++;
+
+    $catDisplayEl.find('.cat-counter').text(catObj.clicksNum);
 });
