c2825a2e66286c882d96950d566cb9ccb944cc8e
Anlh <heldern19@gmail.com>
2018-10-08 21:43:37 +0100

    -> Add a new feature to the click cat premium, now its called click cat premium pro!

--- app.js
+++ app.js
@@ -28,12 +28,18 @@ $(function () {
             // initialize views
             catListView.init();
             catView.init();
+            adminView.init();
         },
         getAllCats() {
             return model.cats;
         },
         setCurrentCat(cat) {
             model.currentCat = cat;
+        },
+        editCurrentCat(name, imgUrl, countClick) {
+            model.currentCat.name = name;
+            model.currentCat.img = imgUrl;
+            model.currentCat.countClick = countClick;
         },
         getCurrentCat() {
             return model.currentCat;
@@ -57,6 +63,8 @@ $(function () {
             this.render();
         },
         render() {
+            this.$catUlListEl.html('');
+
             octopus.getAllCats().forEach(cat => {
                 $('<li/>', {
                     text: cat.name
@@ -65,6 +73,7 @@ $(function () {
                     .click(() => {
                         octopus.setCurrentCat(cat);
                         catView.render();
+                        adminView.render();
                     });
             });
         }
@@ -73,18 +82,54 @@ $(function () {
     const catView = {
         init() {
             this.$catDisplayEl = $('.cat-display-area');
+            this.$catNameEl = this.$catDisplayEl.find('#cat-name-js');
+            this.$catImageEl = this.$catDisplayEl.find('#cat-img-js');
+            this.$catCountEl = this.$catDisplayEl.find('#cat-counter-js');
 
-            this.$catDisplayEl.on('click', 'img', () => octopus.incrementCounter());
+            this.$catImageEl.click(() => octopus.incrementCounter());
 
             this.render();
         },
         render() {
             const {name, countClick, img} = octopus.getCurrentCat();
-            this.$catDisplayEl.html(
-                `<h2>${name}</h2>
-                <img width="400px" src="${img}"><br>
-                <span class="cat-counter">${countClick}</span> Meows!`
-            );
+            this.$catNameEl.text(name);
+            this.$catImageEl.attr('src', img);
+            this.$catCountEl.text(countClick);
+        }
+    };
+
+    const adminView = {
+        init() {
+            this.$adminBtnEl = $('#admin-js');
+            this.$adminPanelEl = $('#admin-panel-js');
+            this.$editNameEl = $('#edit-name-js');
+            this.$editImgUrlEl = $('#edit-image-url-js');
+            this.$editNumClicksEl = $('#edit-num-clicks-js');
+            this.$submitBtnEl = $('#edit-cat-btn-js');
+
+            this.$adminBtnEl.click(() => this.$adminPanelEl.toggle());
+
+            this.$submitBtnEl.click(event => {
+                event.preventDefault();
+
+                octopus.editCurrentCat(
+                    this.$editNameEl.val(),
+                    this.$editImgUrlEl.val(),
+                    this.$editNumClicksEl.val()
+                );
+
+                this.$adminPanelEl.hide();
+                catView.render();
+                catListView.render();
+            });
+
+            this.render();
+        },
+        render() {
+            const {name, img, countClick} = octopus.getCurrentCat();
+            this.$editNameEl.val(name);
+            this.$editImgUrlEl.val(img);
+            this.$editNumClicksEl.val(countClick);
         }
     };
 
